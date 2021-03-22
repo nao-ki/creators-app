b58633c674afc76af0204c0cffa4f13b2772747e
nao-ki <naoki19880817@gmail.com>
2021-03-22 18:47:29 +0900

    home-index

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,23 +2,12 @@ class HomeController < ApplicationController
   before_action :move_to_index
 
   def index
-    @writes = Write.includes(:user).order("created_at DESC").limit(6)
-    @images = Image.order("RAND()").limit(6)
 
 
-    @userimage = Image.all.includes(:user).order("created_at DESC").limit(6)
-
-    @videos = Video.includes(:user).order("created_at DESC").limit(6)
-
-    @sounds = Sound.order("created_at DESC").limit(6)
-    @comment = Comment.new
-    @like = Like.new
-
-    # @user = User.find(2)
   end
 
   def slide
-    @images = Image.includes(:user).order("RANDOM()").limit(6)
+    # @images = Image.includes(:user).order("RANDOM()").limit(6)
   end
 
   def about
